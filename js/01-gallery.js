5fa24f7ccea38e09e6f1a525211778da2c48d1cb
OlgaTikhonchik <olga882011@gmail.com>
2023-03-02 19:26:43 +0200

    Завершение работы

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+       </a>
+    </div>`;
+    })
+    .join('');
+}
+
+function onGalleryImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+ `);
+
+  instance.show();
+
+  window.addEventListener('keydown', onEscKeyPress);
+
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
